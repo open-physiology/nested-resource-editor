634bc866d5201f06bb600b90ea66cffce6d29eb8
mhelvens <mhelvens@gmail.com>
2017-04-12 17:35:31 +0100

    tying some loose ends: test docs + generated code

--- dist/project-template-minimal.js
+++ dist/project-template-minimal.js
@@ -88,9 +88,9 @@ Object.defineProperty(exports, "__esModule", {
 exports.plus = plus;
 /**
  * An excellent function that adds two numbers together.
- * @param   {number}  a  The first number.
- * @param   {number}  b  The second number.
- * @returns {number}     a + b
+ * @param {number} a - the first number
+ * @param {number} b - the second number
+ * @returns {number} - the result: a + b
  * @example
  * let x = plus(10, 20);
  * expect(x).to.equal(30);
